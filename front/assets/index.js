93be611f7b32c119b94c70ea4d740d6a0316b2f9
MoenesMannaii <moenes.samp55@gmail.com>
2024-08-13 13:14:00 +0100

    Pull iheb's changes

--- front/assets/index.js
+++ front/assets/index.js
@@ -1,9 +1,11 @@
 import Logo from './images/Logo.png'
 import imgCarousel1 from './images/1.webp'
 import imgCarousel2 from './images/2.webp'
+import Flower from './images/flower.webp'
 
 export {
     Logo,
     imgCarousel1,
-    imgCarousel2
+    imgCarousel2,
+    Flower
 }
