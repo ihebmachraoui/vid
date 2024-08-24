df2a76fdeb6babb90396c9695213bb09916d60e8
Moenes Mannai <132056443+MoenesMannaii@users.noreply.github.com>
2024-08-24 15:21:14 +0100

    Update page.js Blogs

--- front/app/blog/page.js
+++ front/app/blog/page.js
@@ -1,185 +1,170 @@
-"use client"
-import React from "react";
-import {useState , useEffect} from "react";
-import Image from "next/image";
-import Button from "../../constants/Button/Button.jsx"
-import axios  from 'axios';
-function page() {
-	
-	const truncateText = (text, wordLimit) => {
-		const words = text.split(" ");
-		if (words.length > wordLimit) {
-			return words.slice(0, wordLimit).join(" ") + "...";
-		}
-		return text;
-	};
-	const [blogs, setBlogs] = useState([]);
+"use client";
+import React, { useState, useEffect } from "react";
+import * as images from "../../assets/index";
+import Button from "../../constants/Button/Button";
+import axios from "axios";
 
-	useEffect(() => {
-	  const fetchBlogs = async () => {
-		try {
-		  const response = await axios.get('https://sociosolution-api.vercel.app/blogs');
-		  setBlogs(response.data);
-		  
-		} catch (error) {
-		  console.error('Error fetching blogs:', error);
-		}
-	  };
-  
-	  fetchBlogs();
-	}, []);
-	
-	
-	return (
-		<>
-			<div
-				className="pt-14 pb-4 sm:pt-20  lg:px-36  lg:flex justify-between items-center"
-				style={{
-					background:
-						"linear-gradient(45deg, rgba(90, 158, 124, 0.8), rgba(131, 204, 97, 0.8))",
-				}}>
-				<p className="text-4xl font-jost capitalize text-white p-4 mt-4 lg:p-5 lg:mt-0">
-					{" "}
-					Blog{" "}
-				</p>
+function Page() {
+  const truncateText = (text = "", wordLimit) => {
+    const words = text.split(" ");
+    if (words.length > wordLimit) {
+      return words.slice(0, wordLimit).join(" ") + "...";
+    }
+    return text;
+  };
 
-				<div>
-					<ol className="flex items-center bg-white opacity-60 rounded-lg p-3 ">
-						<li className="inline-flex items-center">
-							<a
-								className="flex items-center text-sm text-gray-500 hover:text-gray-600 focus:outline-none  dark:text-neutral-500 "
-								href="/">
-								<svg
-									className="shrink-0 me-3 size-4"
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									strokeWidth="2"
-									strokeLinecap="round"
-									strokeLinejoin="round">
-									<path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-									<polyline points="9 22 9 12 15 12 15 22"></polyline>
-								</svg>
-								Home
-							</a>
-							<svg
-								className="shrink-0 mx-2 size-4 text-gray-400 dark:text-neutral-600"
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								strokeWidth="2"
-								strokeLinecap="round"
-								strokeLinejoin="round">
-								<path d="m9 18 6-6-6-6"></path>
-							</svg>
-						</li>
+  const [blogs, setBlogs] = useState([]);
 
-						<li
-							className="inline-flex items-center text-sm font-semibold text-gray-800 truncate dark:text-neutral-200"
-							aria-current="page">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 16 16"
-								fill="currentColor"
-								className="shrink-0 me-3 size-4">
-								<path
-									fillRule="evenodd"
-									d="M11.986 3H12a2 2 0 0 1 2 2v6a2 2 0 0 1-1.5 1.937V7A2.5 2.5 0 0 0 10 4.5H4.063A2 2 0 0 1 6 3h.014A2.25 2.25 0 0 1 8.25 1h1.5a2.25 2.25 0 0 1 2.236 2ZM10.5 4v-.75a.75.75 0 0 0-.75-.75h-1.5a.75.75 0 0 0-.75.75V4h3Z"
-									clipRule="evenodd"
-								/>
-								<path
-									fillRule="evenodd"
-									d="M3 6a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H3Zm1.75 2.5a.75.75 0 0 0 0 1.5h3.5a.75.75 0 0 0 0-1.5h-3.5ZM4 11.75a.75.75 0 0 1 .75-.75h3.5a.75.75 0 0 1 0 1.5h-3.5a.75.75 0 0 1-.75-.75Z"
-									clipRule="evenodd"
-								/>
-							</svg>
-							Blogs
-						</li>
-					</ol>
-				</div>
-			</div>
-{ blogs.length <0 ? (
-<p className="text-5xl">no blogs for now</p>
-):(
-			<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8 lg:p-28">
-				{blogs.map((blog, index) => (
-					<div key={index} className="">
-						<div>
-							<a href={`/blog/${blog._id}`} className="block relative no-underline">
-								<div className="relative">
-									<span className="absolute top-3 right-2 leading-4 bg-[#ffb7cec6] rounded-md text-sm text-white p-2 uppercase">
-										{blog.type}
-									</span>
-									<img src={blog.src} alt="Image" className="w-full h-56 object-cover rounded-lg" />
-								</div>
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await axios.get(
+          "https://sociosolution-api.vercel.app/blogs"
+        );
+        setBlogs(response.data);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    };
+    fetchBlogs();
+  }, []);
 
-								<h1 className="font-jost text-xl text-secondary capitalize font-semibold pt-6">
-									{blog.title}
-								</h1>
-								<p className="text-third mt-2">
-									{truncateText(blog.description, 20)}
-								</p>
-							</a>
-							<div className="flex items-center pt-2">
-								<img
-									src={blog.usersrc}
-									alt="Image"
-									className="h-7 w-7 ml-2   rounded-full object-cover"
-								/>
-								<p className="text-third font-semibold ml-1 ">
-									{blog.username}
-								</p>
-								<div className="flex ml-5">
-									<svg
-										className="w-[20px] h-[20px] fill-[#648d79] ml-2 mr-1"
-										viewBox="0 0 448 512"
-										xmlns="http://www.w3.org/2000/svg">
-										<path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z"></path>
-									</svg>
-									<p className="text-third text-sm font-extrabold">
-										{blog.date}
-									</p>
-								</div>
-								<svg
-									className="w-[20px] h-[20px] fill-[#648d79] ml-4"
-									viewBox="0 0 512 512"
-									xmlns="http://www.w3.org/2000/svg">
-									<path d="M256 448c141.4 0 256-93.1 256-208S397.4 32 256 32S0 125.1 0 240c0 45.1 17.7 86.8 47.7 120.9c-1.9 24.5-11.4 46.3-21.4 62.9c-5.5 9.2-11.1 16.6-15.2 21.6c-2.1 2.5-3.7 4.4-4.9 5.7c-.6 .6-1 1.1-1.3 1.4l-.3 .3 0 0 0 0 0 0 0 0c-4.6 4.6-5.9 11.4-3.4 17.4c2.5 6 8.3 9.9 14.8 9.9c28.7 0 57.6-8.9 81.6-19.3c22.9-10 42.4-21.9 54.3-30.6c31.8 11.5 67 17.9 104.1 17.9zM128 208a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm128 0a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm96 32a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"></path>
-								</svg>
-								<p className="text-third text-sm font-extrabold">
-									{blog.comments.length}
-								</p>
-							</div>
-						</div>
-					</div>
-				))}
-			</div>
-			)}
-			<div className=" items-center p-4">
-		
-				<div className="">
-				<div className=" flex flex-col justify-center text-center items-center p-6 rounded-lg ">
-  <h2 className="text-5xl font-eduVic font-semibold mb-4">Share Your Story</h2>
-  <p className="text-secondary font-jost ">
-    Do you have a story or issue you'd like to share? Whether it's a personal experience, a challenge you're facing, or something that's been on your mind, this is the place for you. By sharing, you can receive valuable opinions from others and even gain insights from a professional sociologist.
-  </p>
-  <p className="text-gray-400 italic text-sm">
+  const categories = [
+    "Environment and Society",
+    "Family",
+    "Relationship",
+    "Health and Illness",
+  ];
 
-  Join the conversation, connect, and find support. We're here to listen.
-    </p>
-	<Button text="Add a blog" href={"blog/add-blog"} className="my-4 cursor-pointer"/>
-</div>
+  return (
+    <>
+      <div>
+        <div>
+         
+          <header className="mx-auto max-w-screen-lg rounded-t-lg bg-white pt-16 text-center shadow-lg">
+          
+            <div className="mt-24">            
+            <span className="rounded-full text-black font-medium px-3 mb-2 bg-[#e1ffd4] py-2">
+              Blogs
+            </span>
+              <h1 className="mt-8 text-4xl font-bold text-gray-900 sm:text-5xl">
+              Share Your Story
+            </h1>
+            <p className="lg:block mt-6 text-lg text-gray-700 px-24 hidden ">
+              Do you have a story or issue you'd like to share? Whether it's a
+              personal experience, a challenge you're facing, or something
+              that's been on your mind, this is the place for you. By sharing,
+              you can receive valuable opinions from others and even gain
+              insights from a professional sociologist.
+            </p>
+            </div>
 
-				</div>
-			</div>
-		</>
-	);
+            <div className="mt-6 flex flex-wrap justify-center gap-2">
+              {categories.map((category, index) => (
+                <button
+                  key={index}
+                  className="rounded-lg bg-gray-100 px-2 py-1 font-medium text-gray-600 hover:bg-gray-200"
+                >
+                  {category}
+                </button>
+              ))}
+            </div>
+            <div>
+              <Button
+                text="Add a blog"
+                href={"blog/add-blog"}
+                className="my-4 cursor-pointer"
+              />
+            </div>
+            <img
+              className="-z-10 absolute top-0 left-0 mt-10 h-96 w-full object-cover"
+              src={images.AboutIMG.src}
+              alt=""
+            />
+          </header>
+        </div>
+      </div>
+
+      <div className="w-fit mx-auto mt-10 flex space-x-2">
+        <div className="h-0.5 w-2 bg-gray-600"></div>
+        <div className="h-0.5 w-32 bg-gray-600"></div>
+        <div className="h-0.5 w-2 bg-gray-600"></div>
+      </div>
+
+      <aside
+        aria-label="Recent Posts"
+        className="mx-auto mt-10 max-w-screen-xl py-20"
+      >
+        <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+          <div className="mb-10 md:mb-16">
+            <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
+              People's Stories
+            </h2>
+            <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
+              ija chouf ! ti ija chouf ness ech yahkiw !
+            </p>
+          </div>
+
+          <div className="grid gap-8 sm:grid-cols-2 sm:gap-12 lg:grid-cols-2 xl:grid-cols-2 xl:gap-16">
+            {blogs.map((blog, index) => (
+              <article
+                key={index}
+                className="flex flex-col items-center gap-4 p-4 rounded-lg bg-white md:flex-row lg:gap-6 transition-transform duration-200 hover:shadow-lg"
+              >
+                <a
+                  href={`/blog/${blog._id}`}
+                  className="group shrink-0 relative block h-56 w-full overflow-hidden rounded-lg bg-gray-100 md:h-24 md:w-24 lg:h-40 lg:w-40"
+                >
+                  <img
+                    src={blog.src}
+                    alt={blog.title}
+                    className="group-hover:scale-110 absolute inset-0 h-full w-full object-cover transition duration-200"
+                  />
+                </a>
+
+                <div className="flex flex-col gap-2 w-full">
+                  <span className="text-sm text-gray-400">
+                    {new Date(blog.date).toLocaleDateString()}
+                  </span>
+                  <h2 className="text-xl font-bold text-gray-800">
+                    <a
+                      href={`/blog/${blog._id}`}
+                      className="hover:text-rose-500 active:text-rose-600 transition duration-100"
+                    >
+                      {blog.title}
+                    </a>
+                  </h2>
+                  <p className="text-gray-500">
+                    {truncateText(blog.description, 15)}
+                  </p>
+                  <div className="flex items-center gap-2 text-sm text-gray-500">
+                    <img
+                      src={blog.usersrc}
+                      alt={blog.username}
+                      className="h-6 w-6 rounded-full object-cover"
+                    />
+                    <span>{blog.username}</span>
+                    <span className="text-gray-400">|</span>
+                    <span>{blog.type}</span>
+                    <span className="text-gray-400">|</span>
+                    <span>{blog.comments.length} Comments</span>
+                  </div>
+                  <div>
+                    <Button
+                      href={`/blog/${blog._id}`}
+                      text="Read More"
+                      className="text-xs"
+                   />
+                   
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
+        </div>
+      </aside>
+    </>
+  );
 }
 
-export default page;
+export default Page;
