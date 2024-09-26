5515172fe2fcac74991a61d9d2585ac013be0d98
ihebmachraoui <machraouiiheb19@gmail.com>
2024-09-26 11:41:53 +0100

    Update index.js

--- front/assets/index.js
+++ front/assets/index.js
@@ -1,4 +1,4 @@
-import Logo from './images/Logo.png'
+import Logo from './images/Logo.png' 
 import imgCarousel1 from './images/1.webp'
 import imgCarousel2 from './images/2.webp'
 import Flower from './images/flower.webp'
