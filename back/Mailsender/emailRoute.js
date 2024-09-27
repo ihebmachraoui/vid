c09a764ab72b111cd5cc7dbc3a5559b47ad56a5a
ihebmachraoui <machraouiiheb19@gmail.com>
2024-09-27 05:15:56 +0100

    Delete back/Mailsender directory

--- back/Mailsender/emailRoute.js
+++
@@ -1,9 +0,0 @@
-// emailRoutes.js
-const express = require('express');
-const router = express.Router();
-const { sender } = require('./emailControllers');
-
-// Define the route to send emails
-router.post('/send-email', sender);
-
-module.exports = router;
