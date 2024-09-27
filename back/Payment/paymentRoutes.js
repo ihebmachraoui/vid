dbbb9b8db932cf7fcef8836832679006bef43f7b
ihebmachraoui <machraouiiheb19@gmail.com>
2024-09-27 05:16:06 +0100

    Delete back/Payment directory

--- back/Payment/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generatePayment, verifyPayment } = require('../Payment/paymentControllers');
-
-router.post('/generate-payment', generatePayment);
-router.get('/verify-payment/:id', verifyPayment);
-
-module.exports = router;
